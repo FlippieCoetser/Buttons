f86ad42b195a952da4f3c552938032c56026aaec
Flippie.Coetser <flippie.coetser@gmail.com>
2022-07-14 07:22:24 -0700

    implment demo

--- demo/index.js
+++ demo/index.js
@@ -1,4 +1,7 @@
 import { Event } from '@browser-modules/events';
 let event = new Event()
 
+import { Pin } from '../lib/button.pin.js'
+customElements.define(Pin.tag, Pin)
+
 console.log('Index.js loaded')
