d6ee345b76beb0c89075eff500753b8bb9dea39d
Flippie.Coetser <flippie.coetser@gmail.com>
2022-07-16 10:55:19 -0700

    Add Visibility Attribute unit tests

--- test/button.pin.test.ts
+++ test/button.pin.test.ts
@@ -13,7 +13,7 @@ describe('Given Pin imported', () => {
         expect(Pin.tag).toBe(Tag.Pin)
     })
     it('Then Pin.attribute should be Attribute', () => {
-        expect(Pin.attribute).toBe(Attribute)
+        expect(Pin.attributes).toBe(Attribute)
     })
     describe('Given Pin defined', () => {
         beforeEach(() => {
@@ -57,6 +57,22 @@ describe('Given Pin imported', () => {
                         })
                         it('Then pin.visible should be Visible.NO', () => {
                             expect(pin.visible).toEqual(Visible.NO)
+                        })
+                        it('Then html attribute visible should be Visible.NO', () => {
+                            let visible = pin.getAttribute(Attribute.VISIBLE)
+                            expect(visible).toEqual(Visible.NO)
+                        })
+                        describe('When pin.visible set to Visible.YES', () => {
+                            beforeEach(() => {
+                                pin.visible = Visible.YES
+                            })
+                            it('Then pin.visible should be Visible.YES', () => {
+                                expect(pin.visible).toEqual(Visible.YES)
+                            })
+                            it('Then html attribute visible should be null', () => {
+                                let visible = pin.getAttribute(Attribute.VISIBLE)
+                                expect(visible).toEqual(null)
+                            })
                         })
                     })
                     describe('Given all events has a handler registered', () => {
