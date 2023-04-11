8baf46681e69b6be3c2ae6c59de927aba2b00bd4
Flippie.Coetser <flippie.coetser@gmail.com>
2023-04-11 08:25:09 -0700

    CONFIGURATION: Update Clean Up Task After Unit Testing

--- tasks/clean.test.mjs
+++ tasks/clean.test.mjs
@@ -1,4 +1,4 @@
 #! /usr/bin/env node
 import del from './del.mjs'
-del(['./src/*.js', './src/*.map','./src/utilities/*.js','./src/utilities/*.map']);
+del(['./src/*.js', './src/*.map','./src/utilities/*.js','./src/utilities/*.map','./src/templates/*.js','./src/templates/*.map']);
 del(['./test/*.js', './test/*.map']);
