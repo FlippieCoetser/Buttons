15116baf0d611fd0f0b4ee3c2186e5d7d0bdb10b
Flippie.Coetser <flippie.coetser@gmail.com>
2023-04-11 02:49:06 -0700

    COMPONENT: Update Demo to Leverage TemplateLoader Service

--- demo/index.js
+++ demo/index.js
@@ -1,6 +1,11 @@
-import { Pin } from '../lib/button.pin.js'
-customElements.define(Pin.tag, Pin)
 
+import { TemplateLoader } from './templateLoader.js'
+import { Pin } from '../lib/button.pin.js'
+
+await TemplateLoader.load("../src/templates/button.pin.template.html")
+console.log('button-pin template: Loaded')
+
+customElements.define(Pin.tag, Pin)
 window.pin = document.querySelector('button-pin')
 
-console.log('button-pin: Loaded')
+console.log('button-pin component: Loaded')
