ae63c38ff43c59af7d337a607a312ad84e7a635e
Flippie.Coetser <flippie.coetser@gmail.com>
2023-04-11 06:39:15 -0700

    DOCUMENTATION: Add Note Regarding Imperative Interface

--- demo/index.js
+++ demo/index.js
@@ -6,6 +6,8 @@ await TemplateLoader.load("../lib/templates/button.pin.template.html")
 console.log('button-pin template: Loaded')
 
 customElements.define(Pin.tag, Pin)
+
+// Load Imperative Interface
 window.pin = document.querySelector('button-pin')
 
 console.log('button-pin component: Loaded')
