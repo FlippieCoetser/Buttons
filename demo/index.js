c0b6e57c5a42ae803cce9da232ca9e79ba551a42
Flippie.Coetser <flippie.coetser@gmail.com>
2023-04-13 12:05:48 -0700

    COMPONENT: Add Minimize Button

--- demo/index.js
+++ demo/index.js
@@ -2,6 +2,7 @@
 import { Pin } from '../lib/Button.Pin.js'
 import { Restore } from '../lib/Button.Restore.js'
 import { Close } from '../lib/Button.Close.js'
+import { Minimize} from '../lib/Button.Minimize.js'
 import { TemplateLoader } from '../lib/utilities/templateLoader.js'
 
 await TemplateLoader.load("../lib/templates/Button.Pin.template.html")
@@ -10,10 +11,13 @@ await TemplateLoader.load("../lib/templates/Button.Restore.template.html")
 console.log('button-restore template: Loaded')
 await TemplateLoader.load("../lib/templates/Button.Close.template.html")
 console.log('button-close template: Loaded')
+await TemplateLoader.load("../lib/templates/Button.Minimize.template.html")
+console.log('button-minimize template: Loaded')
 
 customElements.define(Pin.tag, Pin)
 customElements.define(Restore.tag, Restore)
 customElements.define(Close.tag, Close)
+customElements.define(Minimize.tag, Minimize)
 
 // Load Imperative Interface
 window.pin = document.querySelector('button-pin')
@@ -24,3 +28,6 @@ console.log('button-restore component: Loaded')
 
 window.close = document.querySelector('button-close')
 console.log('button-close component: Loaded')
+
+window.minimize = document.querySelector('button-minimize')
+console.log('button-minimize component: Loaded')
---
+++ src/Button.Minimize.ts
@@ -0,0 +1,155 @@
+/**
+* @module Buttons
+*/
+
+import { Component } from "@browser-modules/web.component";
+
+import { Attribute, Event, State, Visible, Handler } from "./Button.metadata.js"
+
+import { Configuration} from "./Button.push.config.js"
+
+/**  
+* @category Components
+*/
+export class Minimize extends Component {
+    /**
+    * @category Attributes
+    */
+    static get attributes() { return Attribute}
+
+    /**
+    * Contains the bindings of UI Gestures and Configuration of the finite state machine.
+    * @hidden
+    */
+    public configuration = Configuration
+    public machine
+    
+    /**
+    * Default visibility is `yes`
+    */
+    private _visible: Visible = Visible.YES
+    /**
+    * Default state is `off`
+    */
+    private _state: State = State.UP
+
+    /**
+    * @hidden
+    */
+    constructor() {
+        super()
+    }
+
+    private _triggerComponentEvent = (event) => 
+        this.dispatchEvent(new CustomEvent(event))
+
+    /**
+    * Id of HTML Template which defaults to `.tag` when no value defined inside component html tag. 
+    * @readonly
+    * @category State
+    */
+    public get templateId() {
+        return this.getAttribute(Attribute.TEMPLATE) ?? Minimize.tag;
+    }
+    
+    /**  
+    * @category State
+    */
+    public get visible(): Visible {
+        return <Visible>this.getAttribute(Attribute.VISIBLE) ?? this._visible;
+    }
+    public set visible(visible: Visible) {
+        this._visible = visible;
+        visible == Visible.YES && this.removeAttribute(Attribute.VISIBLE);
+        visible == Visible.NO && this.setAttribute(Attribute.VISIBLE, visible);
+    }
+
+    /**
+    * Takes any value of the State enumeration related to the specific button type
+    * @category State
+    */
+    public get state(): State { 
+        return <State><unknown>this.getAttribute(Attribute.STATE) ?? this._state;
+    }
+    public set state(state: State) {
+        this._state = state;
+        this.setAttribute(Attribute.STATE, <string><unknown>state);
+    }
+
+    /**
+    * Triggered via `.hide()`
+    * @event
+    * @category Events 
+    */
+    public set onhide(handler: Handler) {
+        this.addEventListener(Event.ONHIDE,handler);
+    }
+        
+    /**
+    * Triggered via `.show()`
+    * @event
+    * @category Events
+    */
+    public set onshow(handler: Handler) {
+        this.addEventListener(Event.ONSHOW,handler);
+    }
+
+    /**
+    * Triggered via `.press()`
+    * @event
+    * @category Events 
+    */
+    public set ondown(handler: Handler) {
+        this.addEventListener(Event.ONDOWN,handler);
+    }
+    
+    /**
+    * Triggered via `.release()`
+    * @event
+    * @category Events
+    */
+    public set onup(handler: Handler) {
+        this.addEventListener(Event.ONUP,handler);
+    }
+
+    /**
+    * @category Operations
+    */
+    public hide = (): void => {
+        if (this.visible !== Visible.NO) {
+            this.visible = Visible.NO
+            this._triggerComponentEvent(Event.ONHIDE)
+        }
+    }
+        
+
+    /**
+    * @category Operations
+    */
+    public show = (): void => {
+        if (this.visible !== Visible.YES) {
+            this.visible = Visible.YES
+            this._triggerComponentEvent(Event.ONSHOW)
+        }
+    }
+
+    /**
+    * @category Operations
+    */
+    public press = (): void => {
+        if (this.state !== State.DOWN) {
+            this.state = State.DOWN
+            this._triggerComponentEvent(Event.ONDOWN)
+        }
+    }
+
+    /**
+    * @category Operations
+    */
+    public release = (): void => {
+        if (this.state !== State.UP) {
+            this.state = State.UP
+            this._triggerComponentEvent(Event.ONUP)
+        }
+    }
+}
