f556338b8eafa02809ac50722f6a02637a330aed
Flippie.Coetser <flippie.coetser@gmail.com>
2023-04-15 10:21:03 -0700

    COMPONENT: Add Button.Content

---
+++ src/Button.Contents.ts
@@ -0,0 +1,166 @@
+/**
+* @module Buttons
+*/
+
+import { Component } from "@browser-modules/web.component";
+import { Machine } from "@browser-modules/machine"
+
+import { States, Attribute, Event, State, Visible, Handler } from "./Button.metadata.js"
+
+import { Configuration} from "./Button.switch.config.js"
+
+/**  
+* @category Components
+*/
+export class Contents extends Component {
+    /**
+    * @category Attributes
+    */
+    static get attributes() { return Attribute}
+
+    /**
+    * Contains the bindings of UI Gestures and Configuration of the finite state machine.
+    * @hidden
+    */
+    public configuration = Configuration
+    public machine
+    
+    /**
+    * Default visibility is `yes`
+    */
+    private _visible: Visible = Visible.YES
+    /**
+    * Default state is `off`
+    */
+    private _state: State = State.OFF
+
+    /**
+    * @hidden
+    */
+    constructor() {
+        super()
+        this.machine = new Machine<Attribute, States, Event>(Configuration)
+        this._registerMachineEvents()
+    }
+
+    private _emitCustomEvent   = (event, data) => 
+        this.dispatchEvent(new CustomEvent(event,{detail:data}))
+
+    private _registerMachineEvents = () => { 
+        this.machine.on(Event.ONHIDE,(visible) => {
+            this.visible = visible
+            this._emitCustomEvent(Event.ONHIDE, { visible: visible })
+        })
+        this.machine.on(Event.ONSHOW,(visible) => {
+            this.visible = visible
+            this._emitCustomEvent(Event.ONSHOW, { visible: visible })
+        })
+        this.machine.on(Event.ONON, (state) => {
+            this.state = state
+            this._emitCustomEvent(Event.ONON, { state: state })
+        })
+        this.machine.on(Event.ONOFF, (state) => {
+            this.state = state
+            this._emitCustomEvent(Event.ONOFF, { state: state })
+        })
+    }
+
+    /**
+    * Id of HTML Template which defaults to `.tag` when no value defined inside component html tag. 
+    * @readonly
+    * @category State
+    */
+    public get templateId() {
+        return this.getAttribute(Attribute.TEMPLATE) ?? Contents.tag;
+    }
+    
+    /**  
+    * @category State
+    */
+    public get visible(): Visible {
+        return <Visible>this.getAttribute(Attribute.VISIBLE) ?? this._visible;
+    }
+    public set visible(visible: Visible) {
+        this._visible = visible;
+        visible == Visible.YES && this.removeAttribute(Attribute.VISIBLE);
+        visible == Visible.NO && this.setAttribute(Attribute.VISIBLE, visible);
+    }
+
+    /**
+    * Takes any value of the State enumeration related to the specific button type
+    * @category State
+    */
+    public get state(): State { 
+        return <State><unknown>this.getAttribute(Attribute.STATE) ?? this._state;
+    }
+    public set state(state: State) {
+        this._state = state;
+        this.setAttribute(Attribute.STATE, <string><unknown>state);
+    }
+
+    /**
+    * Triggered via `.hide()`
+    * @event
+    * @category Events 
+    */
+    public set onhide(handler: Handler) {
+        this.addEventListener(Event.ONHIDE,handler);
+    }
+        
+    /**
+    * Triggered via `.show()`
+    * @event
+    * @category Events
+    */
+    public set onshow(handler: Handler) {
+        this.addEventListener(Event.ONSHOW,handler);
+    }
+
+    /**
+    * Triggered via `.on()`
+    * @event
+    * @category Events 
+    */
+    public set onon(handler: Handler) {
+        this.addEventListener(Event.ONON,handler);
+    }
+    
+    /**
+    * Triggered via `.off()`
+    * @event
+    * @category Events
+    */
+    public set onoff(handler: Handler) {
+        this.addEventListener(Event.ONOFF,handler);
+    }
+
+    /**
+    * @category Operations
+    */
+    public hide = (): boolean => 
+        this.machine.trigger(Event.ONHIDE)
+
+    /**
+    * @category Operations
+    */
+    public show = (): boolean => 
+        this.machine.trigger(Event.ONSHOW)
+
+    /**
+    * @category Operations
+    */
+    public on = (): boolean => 
+        this.machine.trigger(Event.ONON)
+
+    /**
+    * @category Operations
+    */
+    public off = (): boolean => 
+        this.machine.trigger(Event.ONOFF)
+    
+    /**
+    * @category Operations
+    */
+    public toggle = (): boolean =>
+        this.machine.trigger(Event.ONTOGGLE)
+}
---
+++ test/Button.Contents.test.ts
@@ -0,0 +1,275 @@
+import { Utils } from "../src/utilities/utilities.js";
+
+import { Tag } from "../node_modules/@browser-modules/web.component/lib/enums/enum.tag.js"
+
+import { Attribute, Visible, State, Operation } from "../src/Button.metadata.js"
+import { Contents } from "../src/Button.Contents.js";
+
+import { template } from "../src/templates/Button.Contents.template.js"
+
+describe('Given Contents imported', () => {
+    it('Then Contents should be defined', () => {
+        expect(Contents).toBeDefined()
+    })
+    it('Then Contents.tag should be Tag.Contents', () => {
+        expect(Contents.tag).toBe(Tag.Contents)
+    })
+    it('Then Contents.attributes should be Attribute', () => {
+        expect(Contents.attributes).toBe(Attribute)
+    })
+    describe('Given Contents defined', () => {
+        beforeEach(() => {
+            Utils.defineComponent(Contents.tag, Contents)
+        })
+        it('Then custom element registry should contain Contents', () => {
+            expect(customElements.get(Contents.tag)).toBe(Contents)
+        })
+        describe('Given HTML Template added to DOM', () => {
+            let HTMLTemplate: HTMLTemplateElement
+            beforeEach(() => {
+                HTMLTemplate = Utils.appendTemplate(Contents.tag, template)
+            })
+            afterEach(() =>{
+                Utils.removeTemplate(Contents.tag)
+            })
+            it('Then a HTML Template should be available in DOM', () => {
+                expect(document.getElementsByTagName('template')).toHaveSize(1)
+            })
+            describe('When Contents component added to DOM', () => {
+                let contents : Contents;
+                beforeEach(() => {
+                    contents = Utils.appendComponent<Contents>(Contents.tag)
+                })
+                afterEach(() => {
+                    contents.remove()
+                })
+                it('Then contents.templateId should be Contents.tag', () => {
+                    expect(contents.templateId).toBe(Contents.tag)
+                })
+                describe('Given state defaults have been applied', () => {
+                    it('Then contents.visible should be Visible.YES', () => {
+                        expect(contents.visible).toEqual(Visible.YES)
+                    })
+                    it('Then contents.state should be State.OFF', () => {
+                        expect(contents.state).toEqual(State.OFF)
+                    })
+                    describe('Given Imperative API used',() => {
+                        describe('When contents.hide()', () => {
+                            let onhide: jasmine.Spy
+                            let event: CustomEvent
+                            beforeEach(() => {
+                                onhide = jasmine.createSpy('onhide')
+                                contents.onhide = onhide
+                                contents.hide()
+                                event = onhide.calls.mostRecent().args[0]
+                            })
+                            it('Then contents.visible should be Visible.NO', () => {
+                                expect(contents.visible).toEqual(Visible.NO)
+                            })
+                            it('Then html attribute visible should be Visible.NO', () => {
+                                let visible = contents.getAttribute(Attribute.VISIBLE)
+                                expect(visible).toEqual(Visible.NO)
+                            })
+                            it('Then onhide should have been called', () => {
+                                expect(onhide).toHaveBeenCalled()
+                            })
+                            it('Then onhide custom event should have been called with ...args', () => {
+                                expect(event.detail).toEqual({visible: Visible.NO})
+                            })
+                            describe('When contents.show()',() =>{
+                                let onshow: jasmine.Spy
+                                let event: CustomEvent
+                                beforeEach(() =>{
+                                    onshow = jasmine.createSpy('onshow')
+                                    contents.onshow = onshow
+                                    contents.show()
+                                    event = onshow.calls.mostRecent().args[0]
+                                })
+                                it('Then contents.visible should be Visible.YES', () => {
+                                    expect(contents.visible).toEqual(Visible.YES)
+                                })
+                                it('Then html attribute visible should be null', () => {
+                                    let visible = contents.getAttribute(Attribute.VISIBLE)
+                                    expect(visible).toEqual(null)
+                                })
+                                it('Then onshow should have been called once', () => {
+                                    expect(onshow).toHaveBeenCalledTimes(1)
+                                })
+                                it('Then onhide custom event should have been called with ...args', () => {
+                                    expect(event.detail).toEqual({visible: Visible.YES})
+                                })
+                            })
+                        })
+                        describe('When contents.show()', () => {
+                            let onshow: jasmine.Spy
+                            beforeEach(() => {
+                                onshow = jasmine.createSpy('onshow')
+                                contents.onshow = onshow
+                                contents.show()
+                                
+                            })
+                            it('Then contents.visible should be Visible.YES', () => {
+                                expect(contents.visible).toEqual(Visible.YES)
+                            })
+                            it('Then html attribute visible should be null', () => {
+                                let visible = contents.getAttribute(Attribute.VISIBLE)
+                                expect(visible).toEqual(null)
+                            })
+                            it('Then onshow should not have been called', () => {
+                                expect(onshow).not.toHaveBeenCalled()
+                            })
+                            describe('When contents.hide()', () => {
+                                let onhide: jasmine.Spy
+                                let event: CustomEvent
+                                beforeEach(() => {
+                                    onhide = jasmine.createSpy('onhide')
+                                    contents.onhide = onhide
+                                    contents.hide()
+                                    event = onhide.calls.mostRecent().args[0]
+                                })
+                                it('Then contents.visible should be Visible.NO', () => {
+                                    expect(contents.visible).toEqual(Visible.NO)
+                                })
+                                it('Then html attribute visible should be Visible.NO', () => {
+                                    let visible = contents.getAttribute(Attribute.VISIBLE)
+                                    expect(visible).toEqual(Visible.NO)
+                                })
+                                it('Then onhide should have been called once', () => {
+                                    expect(onhide).toHaveBeenCalledTimes(1)
+                                })
+                                it('Then onhide custom event should have been called with ...args', () => {
+                                    expect(event.detail).toEqual({visible: Visible.NO})
+                                })
+                            })
+                        })
+                        describe('When contents.on()', () => {
+                            let onon: jasmine.Spy
+                            let event: CustomEvent
+                            beforeEach(() => {
+                                onon = jasmine.createSpy('onon')
+                                contents.onon = onon
+                                contents.on()
+                                event = onon.calls.mostRecent().args[0]
+                            })
+                            it('Then contents.state should be State.ON', () => {
+                                expect(contents.state).toEqual(State.ON)
+                            })
+                            it('Then html attribute state should be State.ON', () => {
+                                let state = contents.getAttribute(Attribute.STATE)
+                                expect(state).toEqual(State.ON)
+                            })
+                            it('Then onon should have been called', () => {
+                                expect(onon).toHaveBeenCalled()
+                            })
+                            it('Then onon custom event should have been called with ...args', () => {
+                                expect(event.detail).toEqual({state: State.ON})
+                            })
+                            describe('When contents.off()', () => {
+                                let onoff: jasmine.Spy
+                                let event: CustomEvent
+                                beforeEach(() => {
+                                    onoff = jasmine.createSpy('onoff')
+                                    contents.onoff = onoff
+                                    contents.off()
+                                    event = onoff.calls.mostRecent().args[0]
+                                })
+                                it('Then contents.state should be State.OFF', () => {
+                                    expect(contents.state).toEqual(State.OFF)
+                                })
+                                it('Then html attribute state should be State.OFF', () => {
+                                    let state = contents.getAttribute(Attribute.STATE)
+                                    expect(state).toEqual(State.OFF)
+                                })
+                                it('Then onoff should have been called once', () => {
+                                    expect(onoff).toHaveBeenCalledTimes(1)
+                                })
+                                it('Then onoff custom event should have been called with ...args', () => {
+                                    expect(event.detail).toEqual({state: State.OFF})
+                                })
+                            })
+                        })
+                        describe('When contents.off()', () => {
+                            let onoff: jasmine.Spy
+                            beforeEach(() => {
+                                onoff = jasmine.createSpy('onoff')
+                                contents.onoff = onoff
+                                contents.off()
+                            })
+                            it('Then contents.state should be State.OFF', () => {
+                                expect(contents.state).toEqual(State.OFF)
+                            })
+                            it('Then html attribute state should be null', () => {
+                                let state = contents.getAttribute(Attribute.STATE)
+                                expect(state).toEqual(null)
+                            })
+                            it('Then onoff should not have been called', () => {
+                                expect(onoff).not.toHaveBeenCalled()
+                            })
+                            describe('When contents.on()', () => {
+                                let onon: jasmine.Spy
+                                let event: CustomEvent
+                                beforeEach(() => {
+                                    onon = jasmine.createSpy('onon')
+                                    contents.onon = onon
+                                    contents.on()
+                                    event = onon.calls.mostRecent().args[0]
+                                })
+                                it('Then contents.state should be State.NO', () => {
+                                    expect(contents.state).toEqual(State.ON)
+                                })
+                                it('Then html attribute state should be State.ON', () => {
+                                    let state = contents.getAttribute(Attribute.STATE)
+                                    expect(state).toEqual(State.ON)
+                                })
+                                it('Then onoff should not have been called', () => {
+                                    expect(onoff).toHaveBeenCalledTimes(0)
+                                })
+                                it('Then onon custom event should have been called with ...args', () => {
+                                    expect(event.detail).toEqual({state: State.ON})
+                                })
+                            })
+                        })
+                    })
+                    describe('Given User Gestures used', () => {
+                        describe('When click with mouse', () => {
+                            let onon: jasmine.Spy
+                            beforeEach(() => {
+                                onon = jasmine.createSpy('onon')
+                                contents.onon = onon
+                                contents.dispatchEvent(new MouseEvent('click'))
+                            })
+                            it('Then contents.state should be State.ON', () => {
+                                expect(contents.state).toEqual(State.ON)
+                            })
+                            it('Then html attribute state should be State.ON', () => {
+                                let state = contents.getAttribute(Attribute.STATE)
+                                expect(state).toEqual(State.ON)
+                            })
+                            it('Then onon should have been called once', () => {
+                                expect(onon).toHaveBeenCalledTimes(1)
+                            })
+                            describe('When click with mouse', () => {
+                                let onoff: jasmine.Spy
+                                beforeEach(() => {
+                                    onoff = jasmine.createSpy('onoff')
+                                    contents.onoff = onoff
+                                    contents.dispatchEvent(new MouseEvent('click'))
+                                })
+                                it('Then contents.state should be State.OFF', () => {
+                                    expect(contents.state).toEqual(State.OFF)
+                                })
+                                it('Then html attribute state should be State.OFF', () => {
+                                    let state = contents.getAttribute(Attribute.STATE)
+                                    expect(state).toEqual(State.OFF)
+                                })
+                                it('Then onoff should have been called once', () => {
+                                    expect(onoff).toHaveBeenCalledTimes(1)
+                                })
+                            })
+                        })
+                    })
+                })
+            })
+        })
+    })
+})
